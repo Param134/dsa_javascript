4716f8c171059ebdea4a6ea8d423885b7562d179
Param Raval <paramraval411@gmail.com>
2023-12-07 07:39:50 +0000

    Stack update

--- Stack.js
+++ Stack.js
@@ -1,10 +1,46 @@
 //In stack, concept: LIFO(Last In, First Out)
-const stack = [];
+// Stack class
+class Stack {
+	// Array is used to implement stack
+	constructor() {
+		this.items = [];
+	}
 
+push(element) {
+	this.items.push(element);
+}
+
+pop() {
+	if (this.items.length == 0) {
+		return "Underflow";
+    }
+	return this.items.pop();
+}
+
+peek() {
+	return this.items[this.items.length - 1];
+}
+
+isEmpty() {
+	return this.items.length == 0;
+}
+
+printStack() {
+	var str = "";
+	for (var i = 0; i < this.items.length; i++)
+		str += this.items[i] + " ";
+	return str;
+}
+}
+
+var stack = new Stack();
+
+console.log(stack.isEmpty());
+console.log(stack.pop());
 stack.push(10);
 stack.push(20);
-
-console.log(stack);
-
-stack.pop();
-console.log(stack);
+stack.push(30);
+console.log(stack.printStack());
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.printStack());
